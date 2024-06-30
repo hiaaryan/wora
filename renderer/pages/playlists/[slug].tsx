93f9cf22fde5679fd138b19b346a9d54b152085e
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2024-06-30 16:06:46 +0100

    start playlists schema 🎧

--- renderer/pages/playlists/[slug].tsx
+++ renderer/pages/playlists/[slug].tsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/router";
 import { Button } from "@/components/ui/button";
 import { convertTime } from "@/lib/helpers";
 import {
-  IconCircleFilled,
   IconClock,
   IconPlayerPlay,
   IconArrowsShuffle2,
