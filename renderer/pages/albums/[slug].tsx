db67e002e219dbc55ba918981c87ec2480540d56
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2024-07-01 02:36:42 +0100

    fix default input values on update ✌️

--- renderer/pages/albums/[slug].tsx
+++ renderer/pages/albums/[slug].tsx
@@ -167,10 +167,9 @@ export default function Album() {
       <div className="pb-32 pt-2">
         {album &&
           album.songs.map((song, index) => (
-            <ContextMenu>
+            <ContextMenu key={song.id}>
               <ContextMenuTrigger>
                 <div
-                  key={song.id}
                   className="wora-transition flex w-full cursor-pointer items-center justify-between rounded-xl px-4 py-3 hover:bg-white/10"
                   onClick={() => handleMusicClick(index)}
                 >
