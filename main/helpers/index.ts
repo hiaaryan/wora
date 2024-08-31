e27f8497207e9e9223d9636ce9000ffac5ae4b0b
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-08-31 10:19:28 +0700

    remove development code

--- main/helpers/index.ts
+++ main/helpers/index.ts
@@ -3,5 +3,5 @@ import { app } from "electron";
 export * from "./create-window";
 
 // For developing purposes
-app.commandLine.appendSwitch('enable-angle-features', 'disableBlendFuncExtended');
-app.commandLine.appendSwitch('use-angle', 'gl');
+// app.commandLine.appendSwitch('enable-angle-features', 'disableBlendFuncExtended');
+// app.commandLine.appendSwitch('use-angle', 'gl');
